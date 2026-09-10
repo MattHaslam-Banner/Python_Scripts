d12107b7aad94faa4fda2f7390481d498018e94f
George Petch <george.petch@monkhouse.com>
2026-09-10 11:42:45 +0100

    gp test 100926

--- function_app.py
+++ function_app.py
@@ -22,6 +22,6 @@ def ExSWISalesOrderForm(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "GP This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "GP 2 This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
