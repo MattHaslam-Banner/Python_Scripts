0c31b41af3fec679e4452e4754ea7e0fc55c146e
George Petch <george.petch@monkhouse.com>
2026-09-10 15:13:55 +0100

    push

--- function_app.py
+++ function_app.py
@@ -38,7 +38,7 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 @app.route(route="test")
 def test(req: func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse("OK")
+    return func.HttpResponse("OK2")
 
 # def set_secret(secret_value: str, secret_name: str):
 #     """ """
