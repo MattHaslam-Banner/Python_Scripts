d46334579693a38ecf1e0b49b5d8f2353ea6940f
George Petch <george.petch@monkhouse.com>
2026-09-11 09:05:21 +0100

    testing after changing yaml file

--- function_app.py
+++ function_app.py
@@ -2,7 +2,7 @@ import azure.functions as func
 import datetime
 import json
 import logging
-import azure.identity as identity
+from azure.identity import DefaultAzureCredential
 
 
 app = func.FunctionApp()
