2a908570a66667377a4b09e26b15e6bd65b3765a
George Petch <george.petch@monkhouse.com>
2026-09-17 11:27:50 +0100

    email test

--- function_app.py
+++ function_app.py
@@ -458,6 +458,7 @@ def send_URL_email_report(reportLinks: [], emails: str):
     sendgrid_api_key = get_secret("sendgrid-api-key-Nov24")
 
     to_emails = [email.strip() for email in emails.split(";")]
+    to_emails = "george.petch@monkhouse.com"
 
     links_html = ""
 
